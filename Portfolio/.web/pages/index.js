ffcb94e31d5234db091f21085509ae9921d64b28
OWEN LANG <owenlang66@gmail.com>
2023-11-04 14:46:36 -0700

    links

--- Portfolio/.web/pages/index.js
+++ Portfolio/.web/pages/index.js
@@ -3,9 +3,10 @@ import { useRouter } from "next/router"
 import { Event, getAllLocalStorageItems, getRefValue, getRefValues, isTrue, preventDefault, refs, spreadArraysOrObjects, uploadFiles, useEventLoop } from "/utils/state"
 import { ColorModeContext, EventLoopContext, initialEvents, StateContext } from "/utils/context.js"
 import "focus-visible/dist/focus-visible"
-import { Box, Button, Heading, HStack, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, Spacer, Text, VStack } from "@chakra-ui/react"
+import { Badge, Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button, Heading, HStack, Image, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, Spacer, Text, VStack } from "@chakra-ui/react"
 import { getEventURL } from "/utils/state.js"
 import { EmailIcon, MoonIcon, SunIcon } from "@chakra-ui/icons"
+import NextLink from "next/link"
 import NextHead from "next/head"
 
 
@@ -63,9 +64,9 @@ export default function Component() {
   <Fragment/>
 )}
 </Fragment>
-  <VStack sx={{"_light": {"background": "radial-gradient(circle, rgba(0,0,0,0.35) 1px, transparent 1px)", "backgroundSize": "25px 25px"}, "background": "radial-gradient(circle, rgba(255,255,255,0.09) 1px, transparent 1px)", "backgroundSize": "25px 25px"}}>
+  <VStack sx={{"@keyframes dots": {"0%": {"backgroundPosition": "0 0"}, "100%": {"backgroundPosition": "40px 40px"}}, "animation": "dots 4s linear infinite alternate-reverse both", "_light": {"background": "radial-gradient(circle, rgba(0,0,0,0.35) 1px, transparent 1px)", "backgroundSize": "25px 25px"}, "background": "radial-gradient(circle, rgba(255,255,255,0.09) 1px, transparent 1px)", "backgroundSize": "25px 25px"}}>
   <HStack>
-  <HStack alignItems={`center`} justifyContent={`center`}>
+  <HStack alignItems={`center`} justifyContent={`space-between`}>
   <Box>
   <EmailIcon sx={{"_dark": {"color": "rgba(255,255,255,0.5)"}}}/>
 </Box>
@@ -93,11 +94,84 @@ export default function Component() {
   <Box sx={{"width": "100%"}}>
   <Box sx={{"display": ["none", "block", "block", "block"]}}>
   <VStack sx={{"width": "100%", "height": "84vh", "padding": "15rem 0rem", "alignItems": "center", "justifyContent": "start"}}>
-  <HStack>
+  <VStack alignItems={`center`} justifyContent={`center`} spacing={`1.75rem`}>
   <Heading sx={{"fontSize": ["2rem", "2.85rem", "4rem", "5rem", "5rem"], "fontWeight": "900", "_dark": {"background": "linear-gradient(to right, #e1e1e1, #757575)", "backgroundClip": "text"}}}>
   {`Hi there! I'm Owen the former farmer`}
 </Heading>
+  <Heading size={`2xl`} sx={{"@keyframes wave": {"0%": {"transform": "rotate(15deg)"}, "100%": {"transform": "rotate(-15deg)"}}, "animation": "wave 3s cubic-bezier(0.25, 0.46, 0.45, 0.94) infinite alternate-reverse both"}}>
+  {`🍃`}
+</Heading>
+</VStack>
+  <HStack spacing={`1rem`}>
+  <Badge sx={{"padding": ["0.15rem 0.35rem", "0.15rem 0.35rem", "0.15rem 1rem", "0.15rem 1rem", "0.15rem 1rem"]}} variant={`solid`}>
+  {`Software Engineer`}
+</Badge>
+  <Badge sx={{"padding": ["0.15rem 0.35rem", "0.15rem 0.35rem", "0.15rem 1rem", "0.15rem 1rem", "0.15rem 1rem"]}} variant={`solid`}>
+  {`Designer`}
+</Badge>
+  <Badge sx={{"padding": ["0.15rem 0.35rem", "0.15rem 0.35rem", "0.15rem 1rem", "0.15rem 1rem", "0.15rem 1rem"]}} variant={`solid`}>
+  {`Full Stack Developer`}
+</Badge>
 </HStack>
+  <Breadcrumb>
+  <BreadcrumbItem>
+  <HStack>
+  <Image htmlHeight={`24px`} htmlWidth={`24pxx`} src={`/github.png`} sx={{"_dark": {"filter": "brightness(0) invert(1)"}}}/>
+  <BreadcrumbLink as={NextLink} href={`https://github.com/owenlang66`} sx={{"_dark": {"color": "rgba(255,255,255,0.7)"}}}>
+  {`GitHub`}
+</BreadcrumbLink>
+</HStack>
+</BreadcrumbItem>
+  <BreadcrumbItem>
+  <HStack>
+  <Image htmlHeight={`24px`} htmlWidth={`24pxx`} src={`/linkedin.png`} sx={{"_dark": {"filter": "brightness(0) invert(1)"}}}/>
+  <BreadcrumbLink as={NextLink} href={`https://www.linkedin.com/in/owen-lang/`} sx={{"_dark": {"color": "rgba(255,255,255,0.7)"}}}>
+  {`LinkedIn`}
+</BreadcrumbLink>
+</HStack>
+</BreadcrumbItem>
+</Breadcrumb>
+</VStack>
+</Box>
+  <Box sx={{"display": ["block", "none", "none", "none"]}}>
+  <VStack sx={{"width": "100%", "height": "84vh", "padding": "15rem 0rem", "alignItems": "center", "justifyContent": "start"}}>
+  <VStack alignItems={`center`} justifyContent={`center`} spacing={`1.75rem`}>
+  <Heading sx={{"fontSize": ["2rem", "2.85rem", "4rem", "5rem", "5rem"], "fontWeight": "900", "_dark": {"background": "linear-gradient(to right, #e1e1e1, #757575)", "backgroundClip": "text"}}}>
+  {`Hi there! I'm Owen the former farmer`}
+</Heading>
+  <Heading size={`2xl`} sx={{"@keyframes wave": {"0%": {"transform": "rotate(15deg)"}, "100%": {"transform": "rotate(-15deg)"}}, "animation": "wave 3s cubic-bezier(0.25, 0.46, 0.45, 0.94) infinite alternate-reverse both"}}>
+  {`🍃`}
+</Heading>
+</VStack>
+  <VStack spacing={`1.25rem`}>
+  <Badge sx={{"padding": ["0.15rem 0.35rem", "0.15rem 0.35rem", "0.15rem 1rem", "0.15rem 1rem", "0.15rem 1rem"]}} variant={`solid`}>
+  {`Software Engineer`}
+</Badge>
+  <Badge sx={{"padding": ["0.15rem 0.35rem", "0.15rem 0.35rem", "0.15rem 1rem", "0.15rem 1rem", "0.15rem 1rem"]}} variant={`solid`}>
+  {`Designer`}
+</Badge>
+  <Badge sx={{"padding": ["0.15rem 0.35rem", "0.15rem 0.35rem", "0.15rem 1rem", "0.15rem 1rem", "0.15rem 1rem"]}} variant={`solid`}>
+  {`Full Stack Developer`}
+</Badge>
+</VStack>
+  <Breadcrumb>
+  <BreadcrumbItem>
+  <HStack>
+  <Image htmlHeight={`24px`} htmlWidth={`24pxx`} src={`/github.png`} sx={{"_dark": {"filter": "brightness(0) invert(1)"}}}/>
+  <BreadcrumbLink as={NextLink} href={`https://github.com/owenlang66`} sx={{"_dark": {"color": "rgba(255,255,255,0.7)"}}}>
+  {`GitHub`}
+</BreadcrumbLink>
+</HStack>
+</BreadcrumbItem>
+  <BreadcrumbItem>
+  <HStack>
+  <Image htmlHeight={`24px`} htmlWidth={`24pxx`} src={`/linkedin.png`} sx={{"_dark": {"filter": "brightness(0) invert(1)"}}}/>
+  <BreadcrumbLink as={NextLink} href={`https://www.linkedin.com/in/owen-lang/`} sx={{"_dark": {"color": "rgba(255,255,255,0.7)"}}}>
+  {`LinkedIn`}
+</BreadcrumbLink>
+</HStack>
+</BreadcrumbItem>
+</Breadcrumb>
 </VStack>
 </Box>
 </Box>
--- Portfolio/Portfolio/Portfolio.py
+++ Portfolio/Portfolio/Portfolio.py
@@ -7,6 +7,11 @@ import reflex as rx
 class State(rx.State):
     """The app state."""
 
+    dark_mode: bool = False
+
+    def toggle_dark_mode(self):
+        self.dark_mode = not self.dark_mode
+
     pass
 
 dots: dict = {
@@ -15,12 +20,22 @@ dots: dict = {
             "background_position": "0 0"
         },
         "100%": {
-            "background_position": "40pxx 40px"},
+            "background_position": "40px 40px"},
     },
-    "animation": "dots 4s linear infinite alternate-reverse both"
+    "animation": "dots 4s linear infinite alternate-reverse both",
 }
 
-
+wave: dict = {
+    "@keyframes wave": {
+        "0%": {
+            "transform": "rotate(15deg)"
+        },
+        "100%": {
+            "transform": "rotate(-15deg)"
+        },
+    },
+    "animation": "wave 3s cubic-bezier(0.25, 0.46, 0.45, 0.94) infinite alternate-reverse both",
+}
 
 css: dict = {
     "app": {
@@ -63,7 +78,7 @@ class Header:
                         rx.text("owenlang66@gmail.com", _dark={"color": "rgba(255,255,255,0.5)"})
                 ),
             align_items="center",
-            justify_content="center",
+            justify_content="space-between",
         )
         self.theme: rx.Component = rx.color_mode_button(
             rx.color_mode_icon(),
@@ -84,7 +99,7 @@ class Main:
     def __init__(self):
         self.box: rx.Box = rx.box(width="100%")
 
-        self.name: rx.Hstack = rx.hstack(
+        self.name: rx.Vstack = rx.vstack(
             rx.heading(
                 "Hi there! I'm Owen the former farmer",
                 font_size=["2rem", "2.85rem", "4rem", "5rem", "5rem"],
@@ -94,18 +109,82 @@ class Main:
                     "background_clip": "text",
                 },
             ),
+            rx.heading("🍃", size="2xl", style=wave),
+            spacing="1.75rem",
+            align_items="center",
+            justify_content="center",
         )
+
+
+        self.badge_stack_max: rx.Hstack = rx.hstack(spacing="1rem")
+        self.badge_stack_min: rx.Vstack = rx.vstack(spacing="1.25rem")
+        titles: list = ["Software Engineer", "Designer", "Full Stack Developer"]
+        self.badge_stack_max.children = [self.create_badges(title) for title in titles]
+        self.badge_stack_min.children = [self.create_badges(title) for title in titles]
+
+        self.crumbs: rx.Breadcrumb = rx.breadcrumb()
+        data: list = [
+            ["/github.png", "GitHub", "https://github.com/owenlang66"],
+            ["/linkedin.png", "LinkedIn", "https://www.linkedin.com/in/owen-lang/"]
+        ]
+
+        self.crumbs.children = [
+            self.create_breadcrumb_item(path, title, url) for path, title, url in data
+        ]
+ 
+
+    # method: create social links
+    def create_breadcrumb_item(self, path: str, title: str, url: str | None):
+        return rx.breadcrumb_item(
+            rx.hstack(
+                rx.image(src=path, html_width="24pxx", html_height="24px", _dark={"filter": "brightness(0) invert(1)"} if rx.dark_mode() else {},),
+                rx.breadcrumb_link(
+                    title, href=url, 
+                    _dark={"color": "rgba(255,255,255,0.7)"} if rx.dark_mode() else {},
+                ),
+            )
+        )
+
+
+    # method: creating badges
+    def create_badges(self, title: str) -> None:
+        return rx.badge(
+            title,
+            variant="solid",
+            padding=[
+                "0.15rem 0.35rem",
+                "0.15rem 0.35rem",
+                "0.15rem 1rem",
+                "0.15rem 1rem",
+                "0.15rem 1rem"
+                ]
+        )
+
+
 
     def compile_desktop_component(self) -> Component:
         return rx.tablet_and_desktop(
             rx.vstack(
                 self.name,
+                self.badge_stack_max,
+                self.crumbs,
+                style=css.get("main").get("property"),
+            )
+        )
+    
+    def compile_mobile_component(self) -> Component:
+        return rx.mobile_only(
+            rx.vstack(
+                self.name,
+                self.badge_stack_min,
+                self.crumbs,
                 style=css.get("main").get("property"),
             )
         )
     
     def build(self):
-        self.box.children = [self.compile_desktop_component()]
+        self.box.children = [self.compile_desktop_component(),
+                              self.compile_mobile_component()]
         return self.box
 
 
@@ -125,6 +204,7 @@ def landing() -> rx.Component:
         },
         background="radial-gradient(circle, rgba(255,255,255,0.09) 1px, transparent 1px)",
         background_size="25px 25px", 
+        style=dots,
     )
 
 
