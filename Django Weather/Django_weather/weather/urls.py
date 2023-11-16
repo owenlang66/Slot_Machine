9b4fd55bc691bd68fd3657c998d1b0212941ee86
Owen Lang <135743320+owenlang66@users.noreply.github.com>
2023-11-16 10:01:46 -0800

    Delete Django Weather directory

--- Django Weather/Django_weather/weather/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import get_weather, index, result
-
-urlpatterns = [
-    path('', get_weather, name='index'),
-    path('weather/', index, name='weather'),
-]
